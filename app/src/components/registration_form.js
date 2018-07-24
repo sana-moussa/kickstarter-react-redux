a565d0a01a0ac2b377e98862af47eb857fbed96b
Sana Moussa <sana.moussa.88@gmail.com>
2018-07-24 14:18:08 +0200

    added email validation

--- app/src/components/registration_form.js
+++ app/src/components/registration_form.js
@@ -8,15 +8,17 @@ const validate = values => {
   const errors = {};
 
   if (!values.email) {
-    errors.email = "Please enter your email.";
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
   }
 
   if (!values.username) {
-    errors.username = "Please enter your username.";
+    errors.username = "Required";
   }
 
   if (!values.password) {
-    errors.password = "Please choose your password.";
+    errors.password = "Required";
   }
 
   return errors;
